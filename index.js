cd7e3686f62632acf497d0f597b1423c79dc266c
aamin887 <forkahamin@yahoo.co.uk>
2025-04-14 11:39:16 +0100

    resolved conflicts

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 console.log("Hello, world!");
-<<<<<<< HEAD
-=======
 
 const message = "hello again";
 const msg = "testin124";
 
 //Amim test => test amin
->>>>>>> fb5c40275b8c5efd7fc9cf98783678060ac8dd54
